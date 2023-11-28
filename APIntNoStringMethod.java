0440c8860eb6fee80de2de7c84c6832120f85d31
Yang <113082249+1234567Yang@users.noreply.github.com>
2023-11-28 12:31:37 -0800

    Create APIntNoStringMethod.java

---
+++ APIntNoStringMethod.java
@@ -0,0 +1,13 @@
+public class APIntNoStringMethod{
+    public static int IntAtButAPWithoutStringMethod(int number, int pos){
+        int len = IntGetLengthWithoutStringMethod(number);
+        int num1 = (int)(number / Math.pow(10, len - (pos))) * 10;//start with 0
+        int num2 = (int)(number / Math.pow(10, len - (pos + 1)));
+        return num2 - num1;
+    }
+    public static int IntGetLengthWithoutStringMethod(int number){
+        int i = 1;
+        while((number = (int)(number / 10)) != 0) i++;
+        return i;
+    }
+}

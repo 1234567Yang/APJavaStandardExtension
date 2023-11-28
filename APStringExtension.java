cd49b9b11f97289968879750c3e7864f51f9bcf1
Yang <113082249+1234567Yang@users.noreply.github.com>
2023-11-28 12:22:25 -0800

    Update APStringExtension.java

--- APStringExtension.java
+++ APStringExtension.java
@@ -1,4 +1,4 @@
-public static class APStringExtension{
+public class APStringExtension{
     public static int charAtButAPInt(String text, int pos){
         for(int i = 0; i < 256; i++) if(text.substring(pos, pos + 1).compareTo(Character.toString((char)i)) == 0) return i;
         return -1;
